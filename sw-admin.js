e72017249db1f7704a7d3598a69aec13e027afa6
YussXy <yussalmount@gmail.com>
2026-06-11 17:37:03 +0700

    Create sw-admin.js

---
+++ sw-admin.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = 'yussxy-admin-v1';
+var urlsToCache = [
+  '/Baha/admin.html',
+  '/Baha/admin.css',
+  '/Baha/admin.js',
+  '/Baha/image/logo/logo.jpg',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap',
+  'https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function(cache) {
+    return cache.addAll(urlsToCache);
+  }));
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
